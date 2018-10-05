a029aa73e2504d3fdf65df71bec808584f1a39ad
godsby <gg4biz@gmail.com>
2018-10-05 17:21:48 -0400

    well done Leo

---
+++ string_solution.js
@@ -0,0 +1,49 @@
+/*
+//Question 1
+let string = "Hello world";
+console.log(string[0]);
+
+//Question 2
+
+let string = "Hello Pursuit";
+console.log(string.length);
+
+//Question 3
+
+let string = "asdfakshgnc nkjhsdf";
+console.log(string[string.length-1]);
+
+//Question 4
+
+let string = "Nice weekend";
+string = string.slice(0,string.length-1) + string[string.length - 1].toUpperCase();
+console.log(string);
+
+//Question 5
+
+let drEvil = 100000;
+if (drEvil === 1000000) {
+  console.log(drEvil + " dollars " + "(pinky)");
+} else {
+console.log(drEvil + " dollars");
+}
+
+//Question 6
+
+let verbing = "ing";
+if (verbing.length < 3) {
+  console.log(verbing);
+} else if (verbing.slice(-3) === "ing") {
+  verbing += "ly"
+  console.log(verbing);
+} else {
+  verbing += "ing";
+  console.log(verbing);
+}
+
+//Question 7
+
+let withoutLast = "Beautiful day!";
+console.log(withoutLast.slice(0,withoutLast.length - 1));
+
+*/
