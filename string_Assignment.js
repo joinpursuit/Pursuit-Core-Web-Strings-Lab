c74f2b83ed7a1bd99fbe688d63c0ce6155ad6e54
jungraejang <jungraejang@pursuit.org>
2018-10-05 18:07:14 -0400

    string_Assignment

---
+++ string_Assignment.js
@@ -0,0 +1,25 @@
+// Question 1
+// // let name = 'jungrae'
+// // console.log(name[0])
+// //////////////////////////
+// Question 2
+// // let name = jungrae
+// // console.log(name.length)
+// /////////////////////////
+// Question 3
+// // let name = 'jungrae'
+// // console.log(name[name.length - 1])
+// ////////////////////////////
+// Question 4
+// let name = 'jungrae'
+// let upperName = name.slice(0, name.length - 1) + name[name.length - 1].toUpperCase()
+// console.log(upperName);
+//////////////////////////////////////////////////////////////
+// Question 5
+let dollarAmt = 10000
+
+if(dollarAmt === 1000000){
+  console.log(${dollarAmt} + ' dollars' + ' (pinky)')
+} else {
+  console.log(${dollarAmt} + ' dollar')
+}
