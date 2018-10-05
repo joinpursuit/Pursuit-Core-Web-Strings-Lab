b19f22fb08cdc2f5ae2f917d90c3124c70769237
jungraejang <jungraejang@pursuit.org>
2018-10-05 18:09:13 -0400

    string_Assignment.js

--- string_Assignment.js
+++ string_Assignment.js
@@ -1,25 +1,40 @@
-// Question 1
+Question 1
 // // let name = 'jungrae'
 // // console.log(name[0])
 // //////////////////////////
-// Question 2
+Question 2
 // // let name = jungrae
 // // console.log(name.length)
 // /////////////////////////
-// Question 3
+Question 3
 // // let name = 'jungrae'
 // // console.log(name[name.length - 1])
 // ////////////////////////////
-// Question 4
+Question 4
 // let name = 'jungrae'
 // let upperName = name.slice(0, name.length - 1) + name[name.length - 1].toUpperCase()
 // console.log(upperName);
 //////////////////////////////////////////////////////////////
-// Question 5
-let dollarAmt = 10000
-
-if(dollarAmt === 1000000){
-  console.log(${dollarAmt} + ' dollars' + ' (pinky)')
-} else {
-  console.log(${dollarAmt} + ' dollar')
-}
+Question 5
+// let dollarAmt = 10000
+//
+// if(dollarAmt === 1000000){
+//   console.log(${dollarAmt} + ' dollars' + ' (pinky)')
+// } else {
+//   console.log(${dollarAmt} + ' dollar')
+// }
+Question 6
+// let verbing = 'crying'
+//
+// if(verbing.length < 3){
+//   console.log(verbing)
+// } else if(verbing.slice(verbing.length - 3,verbing.length) === 'ing' && verbing.length >= 3){
+//   console.log(verbing + 'ly')
+// } else if(verbing.length >= 3 && verbing.slice(verbing.length - 3,verbing.length) !== 'ing'){
+//   console.log(verbing + 'ing')
+// }
+////////////////////////////////////////////////////////////////
+Question 7
+// let withoutLast = 'withoutLast'
+//
+// console.log(withoutLast.slice(0,withoutLast.length - 4))
