7c7b78cd1923ef23e15cce3df4588f732e306aba
godsby <gg4biz@gmail.com>
2018-10-08 10:44:52 -0400

    another solution

--- WARM-UP-10-08.js
+++ WARM-UP-10-08.js
@@ -1,6 +1,15 @@
+/*
 let str = "Weekdays";
 if (str.length % 2 === 0) {
   console.log( str[str.length / 2 - 1 ] + str[str.length / 2 ])
 } else {
   console.log(str[Math.floor(str.length / 2)]);
+}
+*/
+let str = "pears";
+let midIdex = Math.floor(str.length / 2);
+if (str.length % 2) {
+  console.log(str[midIdex]);
+} else {
+  console.log(str.slice(midIdex -1,midIdex + 1));
 }
