38d2a88beed469383d4eb5c2edfccde3e7bd85cb
Morteza Khaki <mortezakhaki@Mortezas-MacBook-Air.local>
2018-10-08 18:29:11 -0400

    answer

---
+++ string.js
@@ -0,0 +1,49 @@
+// let string = 'morteza';
+// console.log(string[0]);
+//
+// 2.//
+//
+// let string = 'morteza';
+// console.log(string.length);
+//
+// 3.
+// let string = 'morteza';
+//  console.log(string[string.length - 1]);
+
+// 4.
+// let string = 'morteza';
+// let copy = string.slice(string.length - 1);
+// let lastUp = copy.toUpperCase()
+// let copy2 = string.slice(0,string.length - 1)
+// console.log(copy2 + lastUp);
+
+
+//
+// let string = 'morteza';
+// let copy =string.slice(0,string.length - 1) + string[string.length - 1].toUpperCase()
+// console.log(copy);
+
+// let amount = 100
+// if (amount === 1000000) {
+//   console.log(amount + ' dollars ' + 'pinky');
+//
+//   }
+//   else {
+//     console.log(amount + ' dollars ');
+// }
+
+//
+// let str = 'moving';
+//
+// if(str.length < 3) {
+//   console.log(str)
+// } else {
+//   if(str.slice(str.length - 3) === "ing") {
+//     console.log(str + "ly")
+//   } else {
+//     console.log(str + "ing")
+//   }
+// }
+
+// let str = 'morteza';
+// console.log(str.slice(0, str.length - 1));
