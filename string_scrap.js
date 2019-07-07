3c912df80ed061246559c68b0d774337792fedd3
DejaLFlynn <dejaflynn@pursuit.org>
2019-07-06 21:50:11 -0400

    answers to string lab

---
+++ string_scrap.js
@@ -0,0 +1,31 @@
+let word = 'PURSUIT';
+console.log(word[0]);
+
+let str = 'PURSUIT';
+console.log(str.length);
+
+let name = 'PURSUIT';
+let lastCHar = name.charAt(name.length - 1);
+console.log(lastCHar);
+
+let name1 = 'jimmy';
+let lastCHar2 = name1.charAt(name1.length - 1);
+console.log(lastCHar2);
+
+let drEvil = '10';
+if (drEvil[5] === '0' || drEvil [2] === '0') {
+  console.log('pinky');
+} else {
+  console.log('10 dollars');
+}
+
+let strc = 'train';
+let stru = 'box';
+if (strc = 'box' || 'train') {
+  console.log(stru + 'ing');
+} else
+console.log(strc + 'ing');
+
+let str50 = 'go shawty its ya birthday';
+let fifty = str50.slice(10, 25);
+console.log(fifty);
