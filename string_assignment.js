a7efb775056c5c5f2778cfd58a5189fc4fa784e0
shaynadavis <shaynadavis@pursuit.org>
2019-07-08 16:27:45 -0400

    String lab questions completed

---
+++ string_assignment.js
@@ -0,0 +1,66 @@
+//STRING EXERCISES
+
+
+// //1.
+// let myStr = 'You can do it!'
+//   console.log(myStr.charAt(0));
+
+
+// //2.
+// let myStr = 'I did it!'
+//   console.log(myStr.length);
+
+// //3.
+// let myStr = 'Oops I did it again'
+//   console.log(myStr.charAt(myStr.length-1));
+//
+// //4.
+
+// let myStr = 'deon'
+//   console.log(myStr.substring(0, myStr.length-1) + myStr[myStr.length-1].toUpperCase())
+
+//// Just practicing how to make the first character in a string uppercase.
+// let myStr = 'shayna'
+//   console.log(myStr[0].toUpperCase() + myStr.slice(1))
+
+
+
+// //5.
+//
+// for (let drEvil = 10; drEvil <= 1000000000; drEvil *= 10) {
+//     if (drEvil === 1000000000) {
+//       console.log(drEvil + ' dollars pinky')
+//   }else {
+//     console.log(drEvil + ' dollars')
+//   }
+// }
+
+
+
+// //6.
+// let verStr = 'swimming'
+// if (verStr.length < 3) {
+//   console.log(verStr)
+//     }else if (verStr.slice(-3) == 'ing') {
+//       console.log(verStr + 'ly')
+//     } else {
+//         console.log(verStr + 'ing')
+// }
+
+
+// //7.
+// let be = 'say less'
+// let beGone = be.slice(0, -1);
+// console.log(beGone)
+
+
+
+// BONUS ANSWERS
+
+// //1.
+// let numName = 'Ninja'
+// if (numName.length % 2 === 0) {
+//   console.log(numName)
+// } else {
+//   console.log(numName + '#')
+// }
