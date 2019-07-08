8a0ea1223cd711be69fbf3d94e336c59fa55c612
Johanne <johanneenama@gmail.com>
2019-07-07 20:26:59 -0400

    answeredlab

---
+++ strings_lab.js
@@ -0,0 +1,66 @@
+// let str = "johanne";
+// console.log(str[0])
+
+// console.log("---------------")
+//
+// let str = "johanne";
+// console.log(str.length)
+//
+// console.log("-----------------")
+
+// let str = "johanne";
+// console.log(str[str.length - 1])
+
+// console.log("----------------------")
+
+
+// let str = 'johanne';
+// str = str.slice(0, 6) + str[str.length - 1].toUpperCase()
+// console.log(str)
+//
+// let str = 'johanne';
+// str = str.slice(0,str.length-1 ) + str[str.length - 1].toUpperCase()
+// console.log(str)
+
+
+
+
+// console.log("-------------------------")
+//
+// // let variableAmount = 10
+// // if (variableAmount = 1000000) {
+// //   console.log(1000000 + ' dollars' + ' pinky')
+// // }else {
+// //   console.log(10 + ' dollars' )
+// // }
+//
+// console.log("-------------------------")
+//
+// let str = "read"
+// if (str.length < 3) {
+//   console.log("unchanged")
+// } else if (str.length >= 3) {
+//   console.log(str + "ing");
+// } else {
+//   console.log(str + "ly")
+// }
+
+// console.log("--------------------")
+
+// let withoutLast = "It is very hot"
+// // console.log(withoutLast.length)
+// if (copy = withoutLast.slice(0, 10)) {
+// console.log(copy)
+// }
+
+// console.log("-----------------------")
+// Bonus*****
+
+// let str = "summer1"
+// if (str.length % 2 === 1) {
+//   console.log(str + " #")
+// }else{
+// console.log(str)
+// }
+//
+// console.log("-------------------")
