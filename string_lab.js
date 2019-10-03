ddb8a4a27f6d6097b1d5b7516cedeeec3963468d
Kevin Wong <kevinwong@pursuit.org>
2019-10-02 20:36:39 -0400

    Completed problems 5

--- string_lab.js
+++ string_lab.js
@@ -11,6 +11,10 @@ console.log(str1.length);
 console.log(str1[str1.length-1]);
 
 // 4. Create a code block that takes a single string variable, and returns a copy of the string with the last letter capitalized. For example:
+// ```js
+// 'jimmy'
+// // => 'jimmY'
+// ```
 let newStr1 = ""
 for(let i = 0; i <= str1.length-1; i++){
     if(str1[i] !== str1[str1.length-1] ){
@@ -21,10 +25,6 @@ for(let i = 0; i <= str1.length-1; i++){
 }
 console.log(newStr1);
 
-// ```js
-// 'jimmy'
-// // => 'jimmY'
-// ```
 
 // 5. Create a drEvil code block that will take a single number variable, and log the '<variableAmount> dollars',
 // your code block should add '(pinky)' at the end of the amount if it's 1 million. For example:
@@ -34,6 +34,12 @@ console.log(newStr1);
 // let amount = 1000000
 // // =>  1000000 dollars (pinky)
 // ```
+let amount = 808080;
+if (amount >= 1000000){
+    console.log(amount + " dollars (pinky)");
+} else {
+    console.log(amount + " dollars");
+}
 
 
 // 6. Create a `verbing` code block. It should take a single string variable. If its length is at least 3, it should add 'ing' to its end, unless it already ends in 'ing', in which case it should add 'ly' instead. If the string length is less than 3, it should leave it unchanged. 
