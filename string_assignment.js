de8e17b74684e0de1ff673e6f5a0f8f64a5b9be1
Jovanni <jovanniluna@pursuit>
2019-10-05 12:18:12 -0400

    Completed Problems 1-5

---
+++ string_assignment.js
@@ -0,0 +1,17 @@
+//Problem 1
+const string = "the quick brown fox jumped over the lazy dog"
+console.log(string[0])
+//Problem 2
+console.log(string.length)
+//Problem 3
+console.log(string[string.length-1])
+//Problem 4
+console.log(string.slice(0, string.length-1) + string.slice(string.length-1).toUpperCase())
+//Problem 5
+let amount = 100
+const drEvil = `${amount} dollars`
+if(amount >= 1000000){
+    console.log(drEvil + " pinky")
+} else {
+    console.log(drEvil)
+}
