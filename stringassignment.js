2dcbf63becd5730773414f006dc9bb522c895187
Danielle R Cherry <daniellecherry@pursuit.org>
2019-10-05 12:35:49 -0400

    Solve problem 5

--- stringassignment.js
+++ stringassignment.js
@@ -1,21 +1,21 @@
-// # String Exercises
-const group = "Kevin"
+// // # String Exercises
+// const group = "Kevin"
 
-// 1. `console.log` the first character of a string.
-console.log(group[0]);
+// // 1. `console.log` the first character of a string.
+// console.log(group[0]);
 
-// 2. `console.log` the length of a string.
-console.log(group.length)
+// // 2. `console.log` the length of a string.
+// console.log(group.length)
 
-// 3. `console.log` the last character of any string. 
-console.log(group[group.length - 1])
-// 4. Create a code block that takes a single string variable, and returns a copy of the string with the last letter capitalized. For example:
+// // 3. `console.log` the last character of any string. 
+// console.log(group[group.length - 1])
+// // 4. Create a code block that takes a single string variable, and returns a copy of the string with the last letter capitalized. For example:
 
-// ```js
-// 'jimmy'
-// // => 'jimmY'
-// ```
-console.log(group.slice(0,4) + group[4].toUpperCase())
+// // ```js
+// // 'jimmy'
+// // // => 'jimmY'
+// // ```
+// console.log(group.slice(0,4) + group[4].toUpperCase())
 // 5. Create a drEvil code block that will take a single number variable, and log the '<variableAmount> dollars',
 // your code block should add '(pinky)' at the end of the amount if it's 1 million. For example:
 
@@ -25,6 +25,14 @@ console.log(group.slice(0,4) + group[4].toUpperCase())
 // let amount = 1000000
 // // =>  1000000 dollars (pinky)
 // ```
+
+let drEvil = 10000000
+if(drEvil >= 1000000) {
+    console.log(drEvil + " dollars" + "(pinky)");
+} else {
+    console.log(drEvil + " dollars");
+}
+
 
 // 6. Create a `verbing` code block. It should take a single string variable. If its length is at least 3, it should add 'ing' to its end, unless it already ends in 'ing', in which case it should add 'ly' instead. If the string length is less than 3, it should leave it unchanged. 
 // For example:
