7437c1a1fbd2e522e3fbb2673ca8bacf99f83212
RobWalker747 <robwalker@pursuit.org>
2019-10-05 12:15:28 -0400

    completed 1-4

---
+++ index.js
@@ -0,0 +1,6 @@
+// const str = "funhouse"
+// console.(log(str[0]);
+// console.((log(str.length);
+// console.log(str[str.length - 1]);
+const str = "jimmy";
+console.log(str.slice(0,4) + str[4].toUpperCase());
