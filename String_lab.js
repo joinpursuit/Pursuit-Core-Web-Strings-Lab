09c0f94a66f5537b0c4662ab82c7b40f147dfa2b
Dugmar <dugmar.morocho@gmail.com>
2019-10-05 11:53:51 -0400

    First

---
+++ String_lab.js
@@ -0,0 +1,23 @@
+// 1. `console.log` the first character of a string.
+const month = "October";
+
+console.log(month[0]);
+
+// 2. `console.log` the length of a string.
+
+console.log(month.length);
+
+// 3. `console.log` the last character of any string. 
+
+console.log(month[month.length - 1]);
+
+//4. Create a code block that takes a single string variable, and returns a copy of the string with the last letter capitalized. For example:
+let x = month.length -1
+let name =""
+
+for (let i = 0; i < x; i++){
+    if (i<0){
+    name += console.log(month[i])
+    }
+}
+
