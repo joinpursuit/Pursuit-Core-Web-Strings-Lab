7bc8e0125d77b109d5437ddb676bfd24011b0123
rafidhossain1995 <rafidhossain@pursuit.org>
2019-10-05 12:19:47 -0400

    Rafid and Phil

---
+++ string.js
@@ -0,0 +1,17 @@
+
+// problem 1
+let str = "Pursuit"
+console.log(str[0])
+
+    // problem 2
+
+console.log(str.length)
+
+    // problem 3
+
+    console.log(str[str.length -1])
+
+    // problem 4
+
+    let name = "phil"
+    console.log(name.slice(0,3) + name[3].toUpperCase())
