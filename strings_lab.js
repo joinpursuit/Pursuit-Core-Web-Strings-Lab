a6f35ec3ab32fff49fb0655b1d1b7ebe9756a658
Oblivion <kiaraedwards@pursuit.org>
2019-10-05 15:38:15 -0400

    almost

---
+++ strings_lab.js
@@ -0,0 +1,115 @@
+// const name = "kiara";
+
+// console.log(name[0]);
+
+// console.log(name.length);
+
+// console.log(name[name.length -1]);
+
+// console.log(name.slice(0,4) + name[4].toUpperCase());
+
+
+
+// let drEvil = 1000000
+
+// if( drEvil === 1000000){
+//     console.log(`${drEvil} dollars pinky`)
+// }else{
+    
+//     console.log(`${drEvil} dollars `)
+// }
+
+
+
+// let verbing =  "laughing"
+
+// if (verbing.length >= 3){
+//     console.log(verbing + "ing")
+// }else if(verbing.split(verbing.length >= 4)){
+//     console.log(verbing + "ly")
+// } 
+
+
+
+
+// let sentence = 'everyone is funny';
+
+
+// for (let i = sentence.length -6; i >= 0; i -= 1){
+//     console.log(sentence[i])
+// }
+
+
+// console.log(sentence.split('funny'));
+
+
+
+
+
+
+
+
+
+
+// BONUS QUESTIONS
+
+// let name = "erik";
+
+
+// if(name.length % 2=== 1){
+//     console.log(name + "#")
+// }else {
+//     console.log(name)
+// }
+
+
+// let word = "go";
+// //  let sentence = "she is"
+
+// if (word.length >=3){
+//     console.log(word.slice(word.length -3))
+// }else {
+//     console.log("error")
+// }
+
+
+
+// let x = "orange";
+
+// if(x[0] === "a" || x[0] === "i" || x[0] === "o" || x[0]=== "e" || x[0]=== "u"){
+// console.log( x + " " + "starts with a vowel.")
+// }
+
+    
+
+// let str = "hello";
+// let str2 = "world";
+
+
+// console.log(str.slice(0,3) + str2.slice(0,2) + str.slice(2,5) + str2.slice(1,5));
+
+
+
+// let x = "jjklsdwq";
+
+// if( x.split('a') ||   x.split('i') ||   x.split('o') ||  x.split('e') ||  x.split('u')){
+// console.log( x + " " + "has a vowel.")
+// } else{
+//     console.log(x + " " + "has no vowel")
+// }
+
+
+
+
+let x = "apple";
+
+for (let i = 0; i<x.length; i ++){
+    console.log(x[i])
+}
+
+
+
+
+
+
+
