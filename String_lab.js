771bcef688426d3efbc351bafce574adf2116c85
Dugmar <dugmar.morocho@gmail.com>
2019-10-05 12:25:37 -0400

    complete problem 5

--- String_lab.js
+++ String_lab.js
@@ -17,3 +17,13 @@ console.log(day.length)
 //  4. Create a code block that takes a single string variable, and returns a copy of the string with the last letter capitalized.
 
 console.log(day.slice(0, [day.length-1]) + day[day.length - 1].toUpperCase());
+
+// 5. Create a drEvil code block that will take a single number variable, and log the '<variableAmount> dollars',
+// your code block should add '(pinky)' at the end of the amount if it's 1 million.
+
+let num = 10000;
+if (num <= 1000000) {
+    console.log(num + ' dollars');
+} else {
+    console.log(num + ' dollars (pinky)');
+}
