374f771bf4cd2c348c3317f46bd5899a30e1493c
Jovanni <jovanniluna@pursuit>
2019-10-05 16:48:45 -0400

    Completed up to Problem 4

--- string_assignment.js
+++ string_assignment.js
@@ -26,10 +26,35 @@ console.log(verb + "ing")
 }
 
 // Problem 7
-const sentence = "I was late to class"
+const sentence = "Let's remove the last word in this sentence"
 for (i=sentence.length-1; i >= 0; i--){
     if(sentence[i] === " ") {
         console.log(sentence.slice(0,i))
         break
     }
 }
+
+//Bonus 1
+let string1 = "The cat"
+if(string1.length%2 !== 0){
+    console.log(string1 + "#")
+} else console.log(string1)
+
+//Bonus 2
+let string2 = "heigt2"
+if(string2.length < 3){
+    console.log("error")
+} else  console.log(string2.slice( string2.length - 3, string2.length))
+
+//Bonus 3
+let x = "ello"
+let compare = x[0].toUpperCase()
+if(compare === "A" || compare === "E" || compare === "I" || compare === "O" || compare === "U"){
+    console.log("String starts with a vowel")
+    } else console.log("Does not contain a vowel")
+
+//Bonus 4
+let str1 = "123456"
+let str2 = "0"
+let middle = Math.round(str1.length/2)
+console.log(str1.slice(0, middle) + str2 + str1.slice(middle))
