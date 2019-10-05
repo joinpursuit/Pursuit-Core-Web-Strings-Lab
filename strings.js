5a96fc64f057abc16664b8c4b9ff200d525bfbbf
Henrynunez112 <henrynunez@pursuit.org>
2019-10-05 15:14:58 -0400

    string assignment2

--- strings.js
+++ strings.js
@@ -1,28 +1,32 @@
-let firstString = "marathon";
-// console.log(firstString[0])
-// console.log(firstString.length)
-// console.log(firstString[firstString.length-1])
-//console.log(firstString.slice(0,7).toLowerCase() + firstString[firstString.length-1].toUpperCase())
-let varAmount = 6
-let currency = 'dollars'
-console.log(`${varAmount} ${currency} hahahaha (pinky)`)
+// let firstString = "marathon";
+// // console.log(firstString[0])
+// // console.log(firstString.length)
+// // console.log(firstString[firstString.length-1])
+// //console.log(firstString.slice(0,7).toLowerCase() + firstString[firstString.length-1].toUpperCase())
+// let varAmount = 6
+// let currency = 'dollars'
+// console.log(`${varAmount} ${currency} hahahaha (pinky)`)
 
-let singleString = "number";
-if (singleString.length >= 3){
-    console.log(singleString + "ing")
-}else (
-    console.log(singleString)
-)
-
-
-let withoutLast = "Mo money mo problems"
-console.log(withoutLast.slice())
+// let singleString = "number";
+// if (singleString.length >= 3){
+//     console.log(singleString + "ing")
+// }else (
+//     console.log(singleString)
+// )
 
 
+// let withoutLast = "Mo money mo problems"
+// let copy = withoutLast.slice(0, 11)
+//     console.log(copy)
 
 
+// let codeBlock = "add"
+// if (codeBlock.length % 2 === 1){
+//     console.log(codeBlock + "#")
+// }
 
+let errorCode = "Onesies"
 
-
-
-
+if (errorCode.length <= 3)(
+    console.log("error! error! error!")
+); else (console.log(errorCode [errorCode.length-3]))
