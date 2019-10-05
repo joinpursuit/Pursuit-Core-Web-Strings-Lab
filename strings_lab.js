09350c08521e9f10fe55ea8269c50b5ef66fc7e8
Oblivion <kiaraedwards@pursuit.org>
2019-10-05 16:16:20 -0400

    done

--- strings_lab.js
+++ strings_lab.js
@@ -101,11 +101,12 @@
 
 
 
-let x = "apple";
-
-for (let i = 0; i<x.length; i ++){
-    console.log(x[i])
-}
+// let x = "jjtw";
+//  if(x.includes("a") + x.includes("e") + x.includes("i")+ x.includes("o") +  x.includes("u")){
+//      console.log("has a vowel")
+//  }else {
+//      console.log("has no vowel")
+//  }
 
 
 
