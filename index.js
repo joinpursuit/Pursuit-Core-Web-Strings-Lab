cd3d5cf19e79fcc8f5250b6e870249c2e1099787
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2019-10-05 12:37:23 -0400

    complete problem 5 & 6

--- index.js
+++ index.js
@@ -2,5 +2,22 @@
 // console.(log(str[0]);
 // console.((log(str.length);
 // console.log(str[str.length - 1]);
-const str = "jimmy";
-console.log(str.slice(0,4) + str[4].toUpperCase());
+// const str = "jimmy";
+// console.log(str.slice(0,4) + str[4].toUpperCase());
+
+// let amount = 100000;
+// if(amount === 1000000) {
+//     console.log(`${amount} dollars (pinky)`);
+// } else {
+//     console.log(`${amount} dollars`);
+// }
+
+let str = "box";
+let strIndex = str.length - 1;
+if(str.length >= 3 && str[strIndex] === "g" && str[strIndex - 1] === "n" && str[strIndex - 2] === "i") {
+    console.log(`${str}ly`);
+} else if (str.length >= 3) {
+    console.log(`${str}ing`);
+} else {
+    console.log(str);
+}
