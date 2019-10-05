995933708d5d01ee5ee93d8747aad81a360fa6f3
Dugmar <dugmar.morocho@gmail.com>
2019-10-05 12:18:08 -0400

    complete problem 4

--- String_lab.js
+++ String_lab.js
@@ -14,3 +14,6 @@ console.log(day.length)
 
  console.log(day[day.length-1])
 
+//  4. Create a code block that takes a single string variable, and returns a copy of the string with the last letter capitalized.
+
+console.log(day.slice(0, [day.length-1]) + day[day.length - 1].toUpperCase());
