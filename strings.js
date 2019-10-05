41ed3abb62874b071186133ec476731af0e5e26a
Farrah Isabelle <farrahrios@pursuit.org>
2019-10-05 14:24:41 -0400

    solve strings1

---
+++ strings.js
@@ -0,0 +1,28 @@
+let firstString = "marathon";
+// console.log(firstString[0])
+// console.log(firstString.length)
+// console.log(firstString[firstString.length-1])
+//console.log(firstString.slice(0,7).toLowerCase() + firstString[firstString.length-1].toUpperCase())
+let varAmount = 6
+let currency = 'dollars'
+console.log(`${varAmount} ${currency} hahahaha (pinky)`)
+
+let singleString = "number";
+if (singleString.length >= 3){
+    console.log(singleString + "ing")
+}else (
+    console.log(singleString)
+)
+
+
+let withoutLast = "Mo money mo problems"
+console.log(withoutLast.slice())
+
+
+
+
+
+
+
+
+
