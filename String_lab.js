b3f47c34d9febf577343e967acda4940a882448a
Dugmar <dugmar.morocho@gmail.com>
2019-10-05 13:00:23 -0400

    last problem

--- String_lab.js
+++ String_lab.js
@@ -27,3 +27,25 @@ if (num <= 1000000) {
 } else {
     console.log(num + ' dollars (pinky)');
 }
+// 6. Create a `verbing` code block. It should take a single string variable. If its length is at least 3, it should add 'ing' to its end, unless it already ends in 'ing', in which case it should add 'ly' instead. If the string length is less than 3, it should leave it unchanged. 
+// For example:
+
+const str = "train"
+
+if (str.length >= 3 && str.slice(str.length-3) === "ing"){
+    console.log(str+ "ly")
+} else if( str.length >= 3){
+    console.log(str + "ing")
+
+}else {
+    console.log(str)
+}
+
+// 7. Create a `withoutLast` code block that takes a single string variable, and returns a copy of the string without the last word.
+const withoutlast = "I like food"
+
+// console.log(withoutlast.split(" ").slice (0, withoutlast.length - 1))
+
+
+let arr = withoutlast.split(" ") 
+console.log(arr.slice (0, arr.length - 1))
