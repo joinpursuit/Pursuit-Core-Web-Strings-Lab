d739d79912c583151afd61f4ad6d48300c16be92
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2019-10-05 15:20:46 -0400

    formatted the solutions

--- codingBatStrings.js
+++ codingBatStrings.js
@@ -1,4 +1,6 @@
 //Problem 1
 //Given a string name e.g. "Bob" return a greeting of the form "Hello Bob!"
-let name = "Bob";
-console.log(`Hello ${name}!`);
+// let name = "Bob";
+// console.log(`Hello ${name}!`);
+
+//Problem 2
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+//PROBLEM 1 2 3
 // const str = "funhouse"
 // console.(log(str[0]);
 // console.((log(str.length);
 // console.log(str[str.length - 1]);
+
+//PROBLEM 4
 // const str = "jimmy";
 // console.log(str.slice(0,4) + str[4].toUpperCase());
 
+//PROBLEM 5
 // let amount = 100000;
 // if(amount === 1000000) {
 //     console.log(`${amount} dollars (pinky)`);
@@ -12,6 +16,7 @@
 //     console.log(`${amount} dollars`);
 // }
 
+//PROBLEM 6
 // let str = "box";
 // let strIndex = str.length - 1;
 // if(str.length >= 3 && str[strIndex] === "g" && str[strIndex - 1] === "n" && str[strIndex - 2] === "i") {
@@ -22,11 +27,13 @@
 //     console.log(str);
 // }
 
+// PROBLEM 7
 // const str = "I love coding";
 // let arr = str.split(" ");
 // arr.splice(arr.length -1);
 // console.log(arr);
 
+// BONUS 1
 // let name = "popi";
 // if(name.length % 2 === 1) {
 //     console.log(`${name}#`);
@@ -34,6 +41,7 @@
 //     console.log(name);
 // }
 
+// BONUS 2
 // const str = "Ro";
 //     if (str.length >= 3) {
 //         console.log(`${str[str.length - 3]}${str[str.length - 2]}${str[str.length - 1]}`)
@@ -41,6 +49,7 @@
 //         console.log("String isn't long enough")
 //     }
 
+// BONUS 3
 // const str = "Ire";
 // if(str[0] === "A" || str[0] === "E" || str[0] === "I" || str[0] === "O" || str[0] === "U") {
 //     console.log(`${str} starts with a vowel`);
@@ -48,11 +57,14 @@
 //     console.log(`${str} doesn't start with a vowel`);
 // }
 
+
+//BONUS 4
 // let str1 = "hello";
 // let str2 = "world";
 // const str1Half = Math.floor(str1.length / 2);
 // console.log(str1.slice(0,str1Half + 1) + str2 + str1.slice(str1Half + 1));
 
+//BONUS 5
 // let str = "taq";
 // let vowel;
 // for (let i = 0; i < str.length; i += 1) {
