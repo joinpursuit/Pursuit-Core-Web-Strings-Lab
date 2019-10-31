058bfd172101ba8bc5455d97cea7de7dedf518ef
bsihame <sihamebazi@pursuit.org>
2019-10-31 18:23:08 -0400

    problems 1 - 3

---
+++ string.js
@@ -0,0 +1,13 @@
+// 1. `console.log` the first character of a string.
+
+let str = "Hello world"
+console.log(str[0]);
+console.log("hello world".slice(0, 1))
+
+// 2. `console.log` the length of a string.
+
+console.log(str.length)
+
+// 3. `console.log` the last character of any string.
+let lastChar = str[str.length - 1]
+console.log(lastChar)
