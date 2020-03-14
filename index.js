c2938f82f53f5ba114c42c283ef854d2606ed3a3
cassidybeni <cassidybeni@pursuit.org>
2020-03-13 23:12:40 -0400

    Complete problem 4

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ console.log(str.length)
 //Problem Three
 
 console.log(str[str.length-1])
+
+//Problem Four
+
+let string = "cassidy"
+let copy = string.slice(6,7).toUpperCase()
+
+console.log(string.slice(0,6) + copy)
