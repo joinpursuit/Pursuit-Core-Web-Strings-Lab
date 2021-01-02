bbc748627f61a27932fab4c2d7d08b052a904661
Jonathan Bedon <bedonjonathan@gmail.com>
2021-01-02 14:52:31 -0500

    Before time

--- problems/strings.js
+++ problems/strings.js
@@ -3,7 +3,9 @@
  * @param {string} - str
  * @returns {string} - first char
  */
-function firstChar() {}
+function firstChar(string) {
+  return string[0]
+}
 
 /**
  * Returns the length of a string
