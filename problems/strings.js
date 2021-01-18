a1dfbea9f44d81a65dfa4b734e95a3e5ac050e3c
Ivan Castillo <total180training@gmail.com>
2021-01-18 04:37:29 -0500

    Final commit. Feel proud to finally understand strings. This was tough

--- problems/strings.js
+++ problems/strings.js
@@ -23,8 +23,8 @@ function firstChar(str) {
   }
 }
 
-console.log(firstChar(""))
-console.log(firstChar("banana"))
+// console.log(firstChar(""))
+// console.log(firstChar("banana"))
 
 /**
  * Returns the length of a string
@@ -38,9 +38,9 @@ function stringLength(str) {
   return str.length
 }
 
-console.log(stringLength("Corey"))
-console.log(stringLength("corey"))
-console.log(stringLength(""))
+// console.log(stringLength("Corey"))
+// console.log(stringLength("corey"))
+// console.log(stringLength(""))
 
 /**
  * Returns the last character of any string
@@ -55,9 +55,9 @@ console.log(stringLength(""))
 function lastLetter(str) {
   return str[str.length - 1] || str
 }
-console.log(lastLetter(""))
-console.log(lastLetter("Corey"))
-console.log(lastLetter("cat"))
+// console.log(lastLetter(""))
+// console.log(lastLetter("Corey"))
+// console.log(lastLetter("cat"))
 
 /**
  * Takes a string and returns a copy of the string with the last
@@ -80,9 +80,9 @@ function capitalizeLastLetter(str) {
   }
 }
 
-console.log(capitalizeLastLetter("Jimmy"))
-console.log(capitalizeLastLetter("cat"))
-console.log(capitalizeLastLetter(""))
+// console.log(capitalizeLastLetter("Jimmy"))
+// console.log(capitalizeLastLetter("cat"))
+// console.log(capitalizeLastLetter(""))
 
 
 /**
@@ -104,8 +104,8 @@ function drEvil(num) {
   }
 }
 
-console.log(drEvil(15))
-console.log(drEvil(1000001))
+// console.log(drEvil(15))
+// console.log(drEvil(1000001))
 
 /**
  * verbing() should take a string. 
@@ -133,9 +133,9 @@ else {
 }
 }
 
-console.log(verbing("swimming"))
-console.log(verbing("box"))
-console.log(verbing("am"))
+// console.log(verbing("swimming"))
+// console.log(verbing("box"))
+// console.log(verbing("am"))
 
 /**
  * Takes a single sentence and returns a copy of the string without the
@@ -154,9 +154,9 @@ function removeLastWord(str) {
   return string
 }
 
-console.log(removeLastWord("Hello"))
-console.log(removeLastWord("Hello world"))
-console.log(removeLastWord("I eat cake"))
+// console.log(removeLastWord("Hello"))
+// console.log(removeLastWord("Hello world"))
+// console.log(removeLastWord("I eat cake"))
 
 
 /**
@@ -182,8 +182,8 @@ return newStr
 
 }
 //Still need to remove the commas in between words after the array is converted into a string
-console.log(everyOtherLetter("Corey"))
-console.log(everyOtherLetter("Skull candy"))
+// console.log(everyOtherLetter("Corey"))
+// console.log(everyOtherLetter("Skull candy"))
 
 
 
@@ -196,21 +196,19 @@ console.log(everyOtherLetter("Skull candy"))
  * example: disemvowel("Hello world!") returns "Hll wrld!"
  */
 function disemvowel(str) {
-let vowel = "aeiou".toUpperCase() + "aeiou"
-let newStr = str
-let chars
-for (let i = 0; i < vowel.length; i++) {
-  chars = newStr.split(vowel[i])
-  console.log(chars)
-  newStr = chars.join('') //at this moment, the newStr is "Crey"
-  console.log(newStr)
-}
-return newStr
+  let vowel = "aeiou".toUpperCase() + "aeiou"
+  let newStr = str
+  let chars
+  for (let i = 0; i < vowel.length; i++) {
+    chars = newStr.split(vowel[i])
+    newStr = chars.join('') //at this moment, the newStr is "Crey"
+  }
+  return newStr
 }
 
-console.log(disemvowel('Corey'))
-console.log(disemvowel("Hello World!"))
-console.log(disemvowel("Hi, I have awesome nice friends"))
+// console.log(disemvowel('Corey'))
+// console.log(disemvowel("Hello World!"))
+// console.log(disemvowel("Hi, I have awesome nice friends"))
 
 
 /**
@@ -222,13 +220,37 @@ console.log(disemvowel("Hi, I have awesome nice friends"))
  * example: disemvowelLongWords("hi i am your friend") returns "hi i am your frnd"
  * example: disemvowelLongWords("amber is the color of your energy") returns "mbr is the clr of your nrgy"
  */
- function disemvowelLongWords(str){
+function disemvowelLongWords(str){
+// Myra's pseudocode
+// create array of words
+// loop through each word
+// for each word:
+// 1. Check if it’s 5 characters or longer
+// If so, update words array with disemveled version
+// otherwise, leave the word alone
 
- }
+  let words = str.split(" ")
+  let newWords = ""
+  
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length >= 5) {
+      newWords = newWords + disemvowel(words[i]) + " "
+    }
+    else {
+      newWords = newWords + words[i] + " "
+    }
 
- console.log(disemvowel('Corey'))
- console.log(disemvowel("Hello World!"))
- console.log(disemvowel("Hi, I have awesome nice friends"))
+  } 
+// The return now outputs a space character at the end. The following 3 lines removes it
+  newWords = newWords.split(" ")
+  newWords.pop()
+  newWords = newWords.join(" ")
+  return newWords
+}
+
+ console.log(disemvowelLongWords('COREY'))
+ console.log(disemvowelLongWords("Hello World!"))
+ console.log(disemvowelLongWords("Hi, I have awesome nice friends"))
 
 
 /**
@@ -240,7 +262,32 @@ console.log(disemvowel("Hi, I have awesome nice friends"))
  * example: middleCharacter("hello") returns "l"
  * example: middleCharacter("ostentatious") returns "ta"
  */
-function middleCharacter() {}
+function middleCharacter(str) {
+  // takes in a string
+  // we need the middle character
+  // split the string into an array
+  // divide the length of array by 2 (array.length / 2) = 2, or 3, or 2.5
+  // if the length of array % 2 === 0 (even), return both middle characters
+  // join these items into a new variable
+  // return new variable with middle character
+
+  let splitStr = str.split("")
+  let divided = Math.floor(splitStr.length / 2)
+  let dividedEven = Math.round((splitStr.length / 2) - 1)
+  let output = []
+  // console.log(divided, splitStr[divided])
+  if (splitStr.length % 2 === 0) {
+    return output = splitStr[dividedEven] + splitStr[divided]
+  }
+  else {
+    return output = splitStr[divided]
+  }
+}
+console.log(middleCharacter("Corey"))
+console.log(middleCharacter("cat"))
+console.log(middleCharacter("kitten"))
+console.log(middleCharacter("miss"))
+
 
 
 /**
@@ -253,7 +300,33 @@ function middleCharacter() {}
  * @param {string} str - an input string
  * @returns {string} - the return value string, reversed str
  */
-function reverseString() {}
+  // takes in a string
+  // I can't use split(), so I won't be directly converting it into an array first
+  // I can still transfer the elements of the string into an array
+  // I need to reverse it, so I need to access the end of the string with str.length-1
+  // I'll need to iterate through the string backwards with a loop, decrementing by 1
+  // I can add every element in the string to a newStr variable as I loop through it
+  // I'll try to do this by setting the reference of the newStr variable with bracket notation
+  // afterwards, I can convert the array back into a string with join
+function reverseString(str) {
+
+
+  let newStr = ""
+  let j = 0
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i]
+    
+    j++
+  }
+
+  return newStr
+}
+
+console.log(reverseString("Hello world"))
+console.log(reverseString("cats are the best"))
+
+console.log("")
 
 /**
  * Takes in a sentence string and reverses each word but not the
@@ -265,7 +338,28 @@ function reverseString() {}
  * example: reverseSentenceWords("i am a racecar") returns "i ma a racecar"
  */
 
-function reverseSentenceWords() {}
+// will take in a string
+// will use the reverseString() function to reverse the entire string and add it to newString
+// will split newString into an array by separating at spaces with .split(" ")
+// will iterate through the array backwards with array.length - 1, i--
+// will add each word to newArray as it iterates backwards
+// the newArray can now be joined into newString and returned
+function reverseSentenceWords(str) {
+  let newString = reverseString(str)
+  let array = newString.split(" ")
+  let newArray = []
+
+  for (let i = array.length - 1; i >= 0; i--) {
+    newArray.push(array[i])
+  }
+  newString = newArray.join(" ")
+  return newString
+}
+
+console.log(reverseSentenceWords("cats are the best"))
+console.log(reverseSentenceWords("Hello World"))
+
+
 
 module.exports = {
   firstChar,
