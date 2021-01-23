6ad0cb3295b640b95431992cc3eb260937f35d81
Rayvon Finney Pinkston <rayfinn55@gmail.com>
2021-01-22 20:26:08 -0500

    2nd commit. almost finished

--- problems/strings.js
+++ problems/strings.js
@@ -2,46 +2,43 @@
  * Returns the first character of a string passed in.
  * @param {string} str - an input string
  * @returns {string} - first char of str
- * 
+ *
  * example: firstChar("banana") returns "b"
  * example: firstChar("pinapple") returns "p"
  * example: firstChar("") returns ""
  */
 function firstChar(str) {
-  if (str.length === 0){
-    return ''
-  } else return str[0]
+  if (str.length === 0) {
+    return "";
+  } else return str[0];
 }
-
 
 /**
  * Returns the length of a string
  * @param {string} str - an input string
  * @returns {number} - length of str
- * 
+ *
  * example: stringLength("what") returns 4
  * example: stringLength("sixteen") returns 7
  */
 function stringLength(str) {
-  return str.length
+  return str.length;
 }
-
-
 
 /**
  * Returns the last character of any string
  * @param {string} - an input string
  * @returns {string} - last letter of the input string
- * 
+ *
  * example: lastLetter("zappa") returns "a"
  * example: lastLetter("sit on a potato pan otis") returns "s"
  * example: lastLetter("") returns ""
  */
 
 function lastLetter(str) {
-  if (str.length === 0){
-    return ''
-  } else return str[str.length-1]
+  if (str.length === 0) {
+    return "";
+  } else return str[str.length - 1];
 }
 
 /**
@@ -49,26 +46,24 @@ function lastLetter(str) {
  * letter capitalized.
  * @param {string} str - an input string
  * @returns {string} - the input string with the last letter capitalized
- * 
+ *
  * example: capitalizeLastLetter("jimmy") returns "jimmY"
  * example: capitalizeLastLetter("what is the meaning of life") returns "what is the meaning of lifE"
  */
 function capitalizeLastLetter(str) {
-  if (str === '') {
-    return ''.toUpperCase()
+  if (str === "") {
+    return "".toUpperCase();
   } else {
-newStr = str[str.length-1].toUpperCase();
+    newStr = str[str.length - 1].toUpperCase();
   }
-  return str.slice(0,str.length-1) + newStr
-  }
-
-
+  return str.slice(0, str.length - 1) + newStr;
+}
 
 /**
  * Create a drEvil function that takes in a number
  * and returns "${number} dollars"
  * if the number is greater than 1 million, add (pinky) at the end
- * 
+ *
  * @param {number} num
  * @returns {string}
  * example: drEvil(10) returns "10 dollars"
@@ -76,16 +71,15 @@ newStr = str[str.length-1].toUpperCase();
  */
 function drEvil(num) {
   if (num >= 1000000) {
-    return (num + " dollars (pinky)")
-  } else 
-  return (num + " dollars")
+    return num + " dollars (pinky)";
+  } else return num + " dollars";
 }
 
 /**
- * verbing() should take a string. 
+ * verbing() should take a string.
  * If its length is >= 3, it should add 'ing' to its end, unless it already ends in 'ing',
- * in which case it should add 'ly' instead. 
- * If the string length is less than 3, it should leave it unchanged. 
+ * in which case it should add 'ly' instead.
+ * If the string length is less than 3, it should leave it unchanged.
  * @param {string} str
  * @returns {string}
  * verbing('box') returns 'boxing'
@@ -94,31 +88,27 @@ function drEvil(num) {
  */
 function verbing(str) {
   if (str.length < 3) {
-    return str
+    return str;
   } else if (str.slice(-3) === "ing") {
-    return str + "ly"
-  } else 
-  return str + "ing"
-} 
-
+    return str + "ly";
+  } else return str + "ing";
+}
 
 /**
  * Takes a single sentence and returns a copy of the string without the
  * last word.
  * @param {string} str - an input string
  * @returns {string} - the return value missing the last word
- * 
+ *
  * example: removeLastWord("Hello!") returns ""
  * example: removeLastWord("I eat cake") returns "I eat"
  */
 function removeLastWord(str) {
- newLine = str.split(' ');
- newLine.pop()
- let string = newLine.join(" ")
- return string
+  newLine = str.split(" ");
+  newLine.pop();
+  let string = newLine.join(" ");
+  return string;
 }
-
-
 
 /**
  * Takes a string and returns a new string that has only the even
@@ -126,73 +116,93 @@ function removeLastWord(str) {
  * Do NOT split the string into an array.
  * @param {string} str - an input string
  * @returns {string} - the return value containing even indexed characters
- * 
+ *
  * example: everyOtherLetter("Corey") returns "Cry"
  * example: everyOtherLetter("Skull candy") returns "Sulcny"
  */
 function everyOtherLetter(str) {
-
+  let newStr = "";
+  for (let i = 0; i < str.length; i += 2) newStr += str[i];
+  return newStr;
 }
-
 
 /**
  * Takes in a string and returns a new string that has all the vowels removed.
  * @param {string} str - an input string
  * @returns {string} - return value string with all vowels removed
- * 
+ *
  * example: disemvowel("aeiou") returns ""
  * example: disemvowel("Hello world!") returns "Hll wrld!"
  */
-function disemvowel() {}
-
+function disemvowel(str) {
+  let newStr = "";
+  let vowel = "aeiouAEIOU";
+  for (let i = 0; i < str.length; i++) {
+    let letter = str[i];
+    if (!vowel.includes(letter)) {
+      newStr += letter;
+    }
+  }
+  return newStr;
+}
 
 /**
  * Takes a string sentence and returns a new sentence.
  * Any word that has a length of 5 or more characters should be disemvoweled.
  * @param {string} str - an input string
  * @returns {string} - return value string
- * 
+ *
  * example: disemvowelLongWords("hi i am your friend") returns "hi i am your frnd"
  * example: disemvowelLongWords("amber is the color of your energy") returns "mbr is the clr of your nrgy"
  */
- function disemvowelLongWords(){}
-
+function disemvowelLongWords(str) {}
 
 /**
  * Takes in a string and returns the middle char.
  * If there is no character in the middle return the middle two characters
  * @param {string} str - an input string
  * @returns {string} - the return value string, either one or two characters
- * 
+ *
  * example: middleCharacter("hello") returns "l"
  * example: middleCharacter("ostentatious") returns "ta"
  */
 function middleCharacter() {}
 
-
 /**
  * Takes in a string and reverses it.
  * Do not use .split()
- * 
+ *
  * example: reverseString("hello") returns "olleh"
  * example: reverseString("sit on a potato pan otis") returns "sito nap otatop a no tis"
- * 
+ *
  * @param {string} str - an input string
  * @returns {string} - the return value string, reversed str
  */
-function reverseString() {}
+function reverseString(str) {
+  let newStr = "";
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    newStr += str[i];
+  }
+  return newStr;
+}
 
 /**
  * Takes in a sentence string and reverses each word but not the
  * order of the words.
  * @param {string} str - an input string
  * @returns {string} - the return value string
- * 
+ *
  * example: reverseSentenceWords("hello world") returns "olleh dlrow"
  * example: reverseSentenceWords("i am a racecar") returns "i ma a racecar"
  */
 
-function reverseSentenceWords() {}
+function reverseSentenceWords(str) {
+  let str = string.split(" ");
+  for (let i = 0; i < str.length; i++) {
+    str[i] = reverseString(str[i]);
+  }
+  return str.join(" ");
+}
 
 module.exports = {
   firstChar,
